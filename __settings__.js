f1b2dac5702c470f6616edeca305ca65c8e22b7e
kelmurzin <85685974+kelmurzin@users.noreply.github.com>
2025-04-17 10:32:22 +0400

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2215269.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 222638846, 222638847, 222664982, 222665154, 222669077, 222683356, 222683476, 222683496, 222687135, 222689448, 223168027, 223266490, 223266701 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/223013447/1/draco.wasm.js', 'wasmUrl' : 'files/assets/223013448/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/223013446/1/draco.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/223015833/1/basis.wasm.js', 'wasmUrl' : 'files/assets/223015831/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/223015832/1/basis.js', 'preload' : false},
+];
